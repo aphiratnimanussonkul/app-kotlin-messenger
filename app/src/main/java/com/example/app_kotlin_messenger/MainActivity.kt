e20c9220b56202924440b9f6e3691511eef1dc5c
Aphirat Nimanussonkul <tata@odds.team>
2020-02-27 11:39:50 +0700

    create setOnClickListener of button_register then get value from input

--- app/src/main/java/com/example/app_kotlin_messenger/MainActivity.kt
+++ app/src/main/java/com/example/app_kotlin_messenger/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.app_kotlin_messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        button_register.setOnClickListener {
+            val username = username_register.text.toString()
+            val email = email_register.text.toString()
+            val password = password_register.text.toString()
+
+            Log.i("Main Activity", "Username: " + username + " Email: " + email + " Password: " + password)
+        }
     }
 }
