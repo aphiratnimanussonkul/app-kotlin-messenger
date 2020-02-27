151d3790d31389862b319846d9004d5c5f5acf39
Aphirat Nimanussonkul <tata@odds.team>
2020-02-27 11:56:26 +0700

    create loginActivity.kt and xml file

---
+++ app/src/main/java/com/example/app_kotlin_messenger/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_kotlin_messenger
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LogInActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
