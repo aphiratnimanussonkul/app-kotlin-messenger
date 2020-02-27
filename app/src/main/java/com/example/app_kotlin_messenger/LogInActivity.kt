3827c4825f396b859820f4ab808ec39c72d06b91
Aphirat Nimanussonkul <tata@odds.team>
2020-02-27 16:05:24 +0700

    created fun performLogIn to do firebase signin with email and password

--- app/src/main/java/com/example/app_kotlin_messenger/LogInActivity.kt
+++ app/src/main/java/com/example/app_kotlin_messenger/LogInActivity.kt
@@ -1,7 +1,10 @@
 package com.example.app_kotlin_messenger
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LogInActivity: AppCompatActivity() {
@@ -13,5 +16,38 @@ class LogInActivity: AppCompatActivity() {
         back_to_register.setOnClickListener {
             finish()
         }
+
+        button_login.setOnClickListener {
+            performLogIn()
+            return@setOnClickListener
+        }
+    }
+
+    private fun  performLogIn() {
+        val email = email_login.text.toString()
+        val password = password_login.text.toString()
+
+        if (email.isEmpty()) {
+            Toast.makeText(baseContext, "Please enter email", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (password.isEmpty()) {
+            Toast.makeText(baseContext, "Please enter password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("LogIn Activity", "Sign In success: ${it.result!!.user!!.uid}")
+                    Toast.makeText(baseContext, "Sign In successfully!", Toast.LENGTH_SHORT).show()
+                    return@addOnCompleteListener
+                }
+            }
+            .addOnFailureListener {
+                Log.w("LogIn Activity", "Sign In failed: ${it.message}")
+                Toast.makeText(baseContext, "Sign In failed!", Toast.LENGTH_SHORT).show()
+                return@addOnFailureListener
+            }
     }
 }
