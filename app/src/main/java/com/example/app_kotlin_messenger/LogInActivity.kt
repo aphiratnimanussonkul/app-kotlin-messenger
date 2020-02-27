3360778b8ecdcb829afbad57b94f216e137c2233
Aphirat Nimanussonkul <tata@odds.team>
2020-02-27 13:50:40 +0700

    listen when Register click then finish activity Login

--- app/src/main/java/com/example/app_kotlin_messenger/LogInActivity.kt
+++ app/src/main/java/com/example/app_kotlin_messenger/LogInActivity.kt
@@ -2,11 +2,16 @@ package com.example.app_kotlin_messenger
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LogInActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        back_to_register.setOnClickListener {
+            finish()
+        }
     }
 }
