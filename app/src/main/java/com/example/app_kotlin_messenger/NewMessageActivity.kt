ffa74f2f065776d0f088d3650005c2f38395f720
Aphirat Nimanussonkul <tata@odds.team>
2020-02-28 16:11:57 +0700

    created adapter by lib Groupie and create UserItem to custom row of recycleview

--- app/src/main/java/com/example/app_kotlin_messenger/NewMessageActivity.kt
+++ app/src/main/java/com/example/app_kotlin_messenger/NewMessageActivity.kt
@@ -2,6 +2,12 @@ package com.example.app_kotlin_messenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
 
@@ -10,5 +16,22 @@ class NewMessageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_new_message)
 
         supportActionBar?.title = "Select User"
+
+        val adapter = GroupAdapter<GroupieViewHolder>()
+
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+        recycle_view_new_message.adapter = adapter
+        recycle_view_new_message.layoutManager = LinearLayoutManager(this)
+    }
+}
+
+class UserItem: Item<GroupieViewHolder>() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) { }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_message
     }
 }
