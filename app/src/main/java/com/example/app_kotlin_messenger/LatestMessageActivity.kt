229dbe997b778d8f41e1f97ef4f9bd4d04969af4
Aphirat Nimanussonkul <tata@odds.team>
2020-02-28 14:03:26 +0700

    checked is user logged in if not redirect to login page:

--- app/src/main/java/com/example/app_kotlin_messenger/LatestMessageActivity.kt
+++ app/src/main/java/com/example/app_kotlin_messenger/LatestMessageActivity.kt
@@ -1,12 +1,29 @@
 package com.example.app_kotlin_messenger
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessageActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_message)
+
+        verifyUserIsLoggedIn()
+    }
+
+    private fun verifyUserIsLoggedIn() {
+        val uid = FirebaseAuth.getInstance().uid
+        if(uid == null) {
+            startLogInActivity()
+        }
+    }
+
+    private fun startLogInActivity() {
+        val intent = Intent(this, LogInActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK.or(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 }
