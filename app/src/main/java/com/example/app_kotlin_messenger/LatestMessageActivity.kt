cdd986c51be164d74fe540dab76aff9fd3aaeddd
Aphirat Nimanussonkul <tata@odds.team>
2020-02-28 13:43:51 +0700

    created new activity LatestMessageActivity

---
+++ app/src/main/java/com/example/app_kotlin_messenger/LatestMessageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.app_kotlin_messenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LatestMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_latest_message)
+    }
+}
